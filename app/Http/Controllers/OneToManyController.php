c5b022a5e6b4d91f1c055a7fbed8f9da09fffda9
Junior Rodrigues <69818760+juniorrodriguesazevedo@users.noreply.github.com>
2021-05-10 16:18:48 -0300

    Update OneToManyController.php

--- app/Http/Controllers/OneToManyController.php
+++ app/Http/Controllers/OneToManyController.php
@@ -11,12 +11,12 @@ class OneToManyController extends Controller
     {
         // $country = Country::where('name', 'Brasil')->get()->first();
         $keySearch = 'a';
-        $countries = Country::where('name', 'LIKE', "%{$keySearch}%")->get();
+        $countries = Country::where('name', 'LIKE', "%{$keySearch}%")->with('states')->get();
 
         foreach ($countries as $country) {
             echo "<b>{$country->name}</b>";
 
-            $states = $country->states()->get();
+            $states = $country->states;
 
             foreach ($states as $state) {
                 echo "<br>{$state->name} - {$state->initials}";
